2f4f57abc9e027839b0245bb80639e9d9025bc0a
wonsukheo <62510983+wonsukheo@users.noreply.github.com>
2021-01-21 22:21:42 -0600

    error fixed

--- Lab3/Lab3/RestaurantBillCalculator.cs
+++ Lab3/Lab3/RestaurantBillCalculator.cs
@@ -1,45 +1,51 @@
 ﻿using System.IO;
-using System;
+
 namespace Lab3
 {
     public static class RestaurantBillCalculator
     {
         public static double CalculateTotalCost(StreamReader input)
         {
-            Console.WriteLine("Please Enter price of your first order: ");
-            double order0 = double.Parse(input.ReadLine());
-            Console.WriteLine("Please Enter price of your second order: ");
-            double order1 = double.Parse(input.ReadLine());
-            Console.WriteLine("Please Enter price of your third order: ");
-            double order2 = double.Parse(input.ReadLine());
-            Console.WriteLine("Please Enter price of your fourth order: ");
-            double order3 = double.Parse(input.ReadLine());
-            Console.WriteLine("Please Enter price of your fifth order: ");
-            double order4 = double.Parse(input.ReadLine());
-            Console.WriteLine("Please Enter Gratuity in percentage: ");
-            double gratuity = double.Parse(input.ReadLine());
+            string orderPrice = input.ReadLine();
+            double order0 = double.Parse(orderPrice);
+
+            orderPrice = input.ReadLine();
+            double order1 = double.Parse(orderPrice);
+
+            orderPrice = input.ReadLine();
+            double order2 = double.Parse(orderPrice);
+
+            orderPrice = input.ReadLine();
+            double order3 = double.Parse(orderPrice);
+
+            orderPrice = input.ReadLine();
+            double order4 = double.Parse(orderPrice);
+
+            string tipPercentage = input.ReadLine();
+            double tip = double.Parse(tipPercentage);
 
             double orderTotal = order0 + order1 + order2 + order3 + order4;
             double orderTax = orderTotal * 0.05;
-            double orderGratuity = (orderTotal + orderTax) * (gratuity / 100);
-            double totalCost = orderTotal + orderTax + orderGratuity;
-            return (int)(totalCost * 100 + 0.5) / 100;
+            double orderTip = (orderTotal + orderTax) * tip / 100;
+            double totalCost = orderTotal + orderTax + orderTip;
+            
+            return (int)(totalCost * 100 + 0.5) / 100.00;
         }
 
         public static double CalculateIndividualCost(StreamReader input, double totalCost)
         {
-            Console.WriteLine("Number of Individual: ");
-            int numOfIndividual = int.Parse(input.ReadLine());
-            double individualCost = (totalCost / numOfIndividual);
+            string number = input.ReadLine();
+            uint numOfIndividuals = uint.Parse(number);
+            double individualCost = (totalCost / numOfIndividuals);
 
-            return (int)(individualCost * 100 + 0.5) / 100;
+            return (int)(individualCost * 100 + 0.5) / 100.00;
         }
 
         public static uint CalculatePayerCount(StreamReader input, double totalCost)
         {
-            Console.WriteLine("Individual bill cost: ");
-            double individualBill = double.Parse(input.ReadLine());
-            uint payerCount = (uint)(totalCost / individualBill * 100 + 0.5) / 100;
+            string number = input.ReadLine();
+            double individualBill = double.Parse(number);
+            uint payerCount = (uint)(totalCost / individualBill)
             return payerCount;
         }
     }
