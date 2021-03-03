1fd216a9d9e758a94be8287658d8e88d6b8f4641
wonsukheo <62510983+wonsukheo@users.noreply.github.com>
2021-03-03 13:20:28 -0600

    stack overflow fixed

--- Lab7/Lab7/lab7.cs
+++ Lab7/Lab7/lab7.cs
@@ -4,34 +4,55 @@ using System.IO;
 namespace Lab7
 {
     public static class Lab7
-    { 
+    {
         public static bool PlayGame(uint[] array)
         {
-            if (array.Length < 2 || array.Length <= array[0] )
+            if (array.Length < 2 || array.Length <= array[0])
             {
                 return false;
             }
 
-            return recursiveFunction(array, array.Length - 1);
+            return RecursiveFunction(array, array.Length - 1);
         }
-        
-        public static bool recursiveFunction(uint[] array, int index)
-        {          
+
+        public static bool RecursiveFunction(uint[] array, int index, int lastindex = 0)
+        {
             // end condition
             if (index == array[0])
             {
                 return true;
+            }
+            int j = 0;
+            while (array[j++] == 0)
+            {
+                if (j == array.Length)
+                {
+                    return false;
+                }
             }
 
             for (int i = 1; i < array.Length - 1; ++i)
             {
                 if (i + array[i] == index || i - array[i] == index)
                 {
-                    return recursiveFunction(array, i);
-                }             
+                    if (i == lastindex)
+                    {
+                        array[index] = 0;
+                        array[lastindex] = 0;
+                        return RecursiveFunction(array, array.Length - 1);
+                    }
+
+                    return RecursiveFunction(array, i, index);
+                }
             }
 
-            return false;
+            if (index == array.Length -1)
+            {
+                return false;
+            }
+
+            array[index] = 0;
+            return RecursiveFunction(array, array.Length - 1);
         }
     }
 }
