36407889c2acc0b6311abddda848e4a8d5341e0e
wonsukheo <62510983+wonsukheo@users.noreply.github.com>
2021-03-14 14:29:35 -0500

    trying to fix b09

--- Lab8/Lab8/Lab8.cs
+++ Lab8/Lab8/Lab8.cs
@@ -25,7 +25,7 @@ namespace Lab8
                 if (firstLevel[i] != null)
                 {
                     prettifyString.Append($"{firstLevelLabel}) ");
-                    firstLevelLabel++;
+
 
                     prettifyString.Append(firstLevel[i]);
 
@@ -35,7 +35,7 @@ namespace Lab8
                     int j = 0;
                     for (; j < prettifyString.Length; j++)
                     {
-                        if (firstLevelLabel != 2 && prettifyString[j] == '_')
+                        if (firstLevelLabel != 1 && prettifyString[j] == '_')
                         {
                             prettifyString.Replace("_", $"\n    {secondLevelLable[secondLevelCount % secondLevelLable.Length]}) ", j, 1);
 
@@ -51,8 +51,8 @@ namespace Lab8
                         {
                             prettifyString.Replace("/", "\n        - ", j, 1);
                         }
-
                     }
+                    firstLevelLabel++;
                     prettifyString.AppendLine();
                 }
             }
